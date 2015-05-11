c9d2a6c03a5f5fc72931874393aec3baf98549dd
Proyecto SIG Corporativo de la Junta de Andalucía <sigcorporativo-ja@users.noreply.github.com>
2015-05-11 13:19:31 +0200

    Cambiado contexto

--- js/config.js
+++ js/config.js
@@ -2,4 +2,4 @@ var prj25830 ="+proj=utm +zone=30 +ellps=GRS80 +units=m +no_defs";
 var urlService="http://api.trackgps.es/ultimo.do";
 var claveAuth="ODp4REhybnY3cEFhK0IxQmI1SFFZeWdRPT0=";
 var idEmp="grea";
-var urlMapeaBase = "http://mapea-sigc.juntadeandalucia.es?wmcfile=http://www.callejerodeandalucia.es/wmc/context_cdau_callejero.xml*Callejero,http://www.callejerodeandalucia.es/wmc/context_cdau_satelite.xml*Satélite";
+var urlMapeaBase = "http://mapea-sigc.juntadeandalucia.es?wmcfile=http://www.juntadeandalucia.es/sandetel/publicacion/planromero2015/context_plan_romero_2015.xml*PlanRomero";
